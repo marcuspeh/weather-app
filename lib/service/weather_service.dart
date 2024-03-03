1836dc5861909cd9bad6210f0e487f44a7408719
Marcus Peh <hkmpeh@gmail.com>
2024-03-03 18:54:54 +0800

    Add location services for weather

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-// import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -9,35 +9,38 @@ import '../models/weather_model.dart';
 class WeatherService {
   static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
+  Location location = Location();
+
   WeatherService();
 
   Future<WeatherModel> getWeather() async {
     _checkAndRequestPermission();
 
-    // final position = await Geolocator.getCurrentPosition();
-    // final responseBody = await getDataFromAPI(position.latitude, position.longitude);
-    final responseBody = await getDataFromAPI(1.2942, 103.7861);
-
+    final locationData = await location.getLocation();
+    final responseBody = await getDataFromAPI(locationData.latitude ?? 0, locationData.longitude ?? 0);
+    
     return WeatherModel.fromJson(responseBody);
   }
 
   Future<void> _checkAndRequestPermission() async {
-    return;
-    // final serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!serviceEnabled) {
-    //   return Future.error('Location services are disabled.');
-    // }
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+    }
 
-    // LocationPermission permission = await Geolocator.checkPermission();
-    // if (permission == LocationPermission.denied) {
-    //   permission = await Geolocator.requestPermission();
-    //   if (permission == LocationPermission.denied) {
-    //     return Future.error('Location permissions are denied');
-    //   }
-    // } else if (permission == LocationPermission.deniedForever) {
-    //   return Future.error(
-    //     'Location permissions are permanently denied, we cannot request permissions.');
-    // } 
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return Future.error('Location permissions are denied');
+      }
+    } else if (permissionGranted == PermissionStatus.deniedForever) {
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
   }
 
   Future<Map<String, dynamic>> getDataFromAPI(double latitude, double longitude) async {
